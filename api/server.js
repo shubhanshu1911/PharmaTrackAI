e5664adc53022919c9ab06af470dc1c2ec96bc21
Yusuf <mohdyusufhesam@gmail.com>
2024-10-10 18:01:03 +0530

    cors added

--- api/server.js
+++ api/server.js
@@ -1,25 +1,27 @@
 const dotenv = require('dotenv');
 const pool = require('./db');
 const express = require('express');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 // Importing routes
 const salesRoutes = require('./routes/salesRoute');
 const orderRoutes = require('./routes/orderRoute');
-const inventoryRoutes = require('./routes/inventoryRoute'); // Add this
-const analyticsRoutes = require('./routes/analyticsRoute'); // Add this
+const inventoryRoutes = require('./routes/inventoryRoute'); 
+const analyticsRoutes = require('./routes/analyticsRoute'); 
 
 // Use routes with a versioned base path
 app.use('/api/v1/sales', salesRoutes);
 app.use('/api/v1/orders', orderRoutes);
-app.use('/api/v1/inventory', inventoryRoutes); // Add this
-app.use('/api/v1/analytics', analyticsRoutes); // Add this
+app.use('/api/v1/inventory', inventoryRoutes); 
+app.use('/api/v1/analytics', analyticsRoutes); 
 
 // Database connection
 pool.connect((err) => {
